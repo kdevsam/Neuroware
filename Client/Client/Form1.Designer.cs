f407c4cb774356e7343ad3de41c47c6daeaa9a6a
kdevsam <kdevsam@gmail.com>
2020-08-05 21:03:14 -0400

    Added C# Inital Client Code

---
+++ Client/Client/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.batCharge = new System.Windows.Forms.Label();
+            this.charger = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // batCharge
+            // 
+            this.batCharge.AutoSize = true;
+            this.batCharge.Location = new System.Drawing.Point(65, 110);
+            this.batCharge.Name = "batCharge";
+            this.batCharge.Size = new System.Drawing.Size(118, 13);
+            this.batCharge.TabIndex = 0;
+            this.batCharge.Text = "Battery Percentage: ?%";
+            // 
+            // charger
+            // 
+            this.charger.AutoSize = true;
+            this.charger.Location = new System.Drawing.Point(65, 74);
+            this.charger.Name = "charger";
+            this.charger.Size = new System.Drawing.Size(109, 13);
+            this.charger.TabIndex = 1;
+            this.charger.Text = "Charger Plugged in: ?";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(58, 126);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(125, 37);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(245, 320);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.charger);
+            this.Controls.Add(this.batCharge);
+            this.Name = "Form1";
+            this.Text = "Neuroware Client";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label batCharge;
+        private System.Windows.Forms.Label charger;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Client/Client/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+
+        int power = 0;
+        Thread db;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread battery = new Thread(ShowPowerStatus);
+            battery.IsBackground = true;
+            battery.Start();
+            db = new Thread(MongoDB);
+            
+        }
+
+        private void ShowPowerStatus()
+        {
+            while (true)
+            {
+                
+                PowerStatus status = SystemInformation.PowerStatus;
+                string pbarstat = status.BatteryLifePercent.ToString("P0").TrimEnd('%');
+                string chargerPlugged = status.PowerLineStatus.ToString();
+                int pbarCount = Int32.Parse(pbarstat);
+                power = pbarCount;
+                if (chargerPlugged.Equals("Online"))
+                {
+                    charger.Invoke(new MethodInvoker(delegate { charger.Text = "SYS is Plugged In"; }));
+                }
+                if(progressBar1.Value != pbarCount)
+                {
+                    progressBar1.Invoke(new MethodInvoker(delegate { progressBar1.Value = pbarCount; }));
+                    db.Start();
+
+                }
+                else
+                {
+                    Thread.Sleep(10000);
+                }
+
+            }
+
+        }
+
+        private void MongoDB()
+        {
+            MongoClient dbClient = new MongoClient("mongodb+srv://user12:9e5XiHWyawGF671n@cluster0.nde7k.mongodb.net/test?retryWrites=true&w=majority");
+            var database = dbClient.GetDatabase("test");
+            var collection = database.GetCollection<BsonDocument>("Systems");
+            //var filter = Builders<BsonDocument>.Filter.Eq("sys_id", 2);
+            //var update = Builders<BsonDocument>.Update.Set("sys_id", 3);
+            //collection.UpdateOne(filter, update);
+
+            var arrayFilter = Builders<BsonDocument>.Filter.Eq("sys_id", 3) & Builders<BsonDocument>.Filter.Eq("Battery.type", "Percent");
+            var arrayUpdate = Builders<BsonDocument>.Update.Set("Battery.$.life", power);
+            collection.UpdateOne(arrayFilter, arrayUpdate);
+
+        }
+    }
+}
